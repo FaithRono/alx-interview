ebcffc240d7b3769de9665621ad676292a857d44
faith rono <faithrono132@gmail.com>
2024-05-28 15:30:09 +0300

    updates

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -13,11 +13,11 @@ def pascal_triangle(n):
     pascal_triangle = [[1]]  # Initialize the first row of the triangle
 
     for integer in range(1, n):
-        pascal_row = [1]  # Start each row with a 1
+        row = [1]  # Start each row with a 1
         for j in range(1, integer):
             # Compute the value based on the two values above it
-            pascal_row.append(pascal_triangle[integer - 1][j - 1] + pascal_triangle[integer - 1][j])
-        pascal_row.append(1)  # End each row with a 1
-        pascal_triangle.append(pascal_row)  # Add the row to the triangle
+            row.append(pascal_triangle[integer - 1][j - 1] + pascal_triangle[integer - 1][j])
+        row.append(1)  # End each row with a 1
+        pascal_triangle.append(row)  # Add the row to the triangle
 
     return pascal_triangle
