25d0666fc6618c6e85975df6dad1b46a5cb3bcf0
faith rono <faithrono132@gmail.com>
2024-05-28 15:36:06 +0300

    updates

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -4,25 +4,20 @@
 
 def pascal_triangle(n):
     """
-    Returns a list of lists of integers representing Pascal's triangle of n.
-
-    Args:
-        n (int): The number of rows in the Pascal's triangle.
-
-    Returns:
-        List[List[int]]: A list of lists of integers representing Pascal's triangle of n.
+    Returns a list of lists of integers
+    representing the Pascal’s triangle of n.
     """
     if n <= 0:
         return []
 
-    pascal_triangle = [[1]]  # Initialize the first row of the Pascal's triangle
+    triangle = [[1]]  # Initialize the first row of the triangle
 
-    for integer in range(1, n):
-        pascal_row = [1]  # Start each row with a 1
-        for j in range(1, integer):
+    for i in range(1, n):
+        row = [1]  # Start each row with a 1
+        for j in range(1, i):
             # Compute the value based on the two values above it
-            pascal_row.append(pascal_triangle[integer - 1][j - 1] + pascal_triangle[integer - 1][j])
-        pascal_row.append(1)  # End each row with a 1
-        pascal_triangle.append(pascal_row)  # Add the row to the Pascal's triangle
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        row.append(1)  # End each row with a 1
+        triangle.append(row)  # Add the row to the triangle
 
-    return pascal_triangle
+    return triangle
