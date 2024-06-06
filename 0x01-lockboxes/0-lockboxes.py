70822e17b1723ecb05731df1e4c9fdade0eaea2a
Faith Cherono <faithrono132@gmail.com>
2024-06-06 09:01:11 +0000

    unlocking boxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -3,6 +3,10 @@
 """
 
 def canUnlockAll(boxes):
+    '''Checks if all the boxes in a list of boxes containing the keys
+    (indices) to other boxes can be unlocked given that the first
+    box is unlocked.
+    '''
     if not boxes:
         return False
 
