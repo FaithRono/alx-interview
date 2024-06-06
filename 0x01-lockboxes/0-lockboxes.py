2ed51479fbbb54753a497dd5a534a4eeb5167a50
Faith Cherono <faithrono132@gmail.com>
2024-06-06 08:52:47 +0000

    unlocking boxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+def canUnlockAll(boxes):
+    if not boxes:
+        return False
+
+    n = len(boxes)  # Number of boxes
+    unlocked = [False] * n  # Track which boxes are unlocked
+    unlocked[0] = True  # The first box is unlocked initially
+
+    stack = [0]  # Use a stack to keep track of boxes to process
+
+    while stack:
+        current_box = stack.pop()  # Get a box to process
+        for key in boxes[current_box]:  # Iterate through the keys in the current box
+            if key < n and not unlocked[key]:  # Check if the key opens a valid and locked box
+                unlocked[key] = True  # Unlock the box
+                stack.append(key)  # Add the newly unlocked box to the stack
+
+    return all(unlocked)  # Return True if all boxes are unlocked, else False
+
