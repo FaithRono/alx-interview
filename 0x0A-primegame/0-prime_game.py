8790dc9607b0f9ca71b2682fdce640d42bd78e02
faith rono <faithrono132@gmail.com>
2024-08-09 00:45:12 +0000

    primegame solved

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,37 +1,44 @@
 #!/usr/bin/python3
+"""
+Define isWineer function, a solution to the Prime Game problem
+"""
 
-def sieve_of_eratosthenes(max_n):
-    sieve = [True] * (max_n + 1)
-    sieve[0] = sieve[1] = False  # 0 and 1 are not prime numbers
-    p = 2
-    while p * p <= max_n:
-        if sieve[p]:
-            for i in range(p * p, max_n + 1, p):
+
+def primes(n):
+    """Return list of prime numbers between 1 and n inclusive
+       Args:
+        n (int): upper boundary of range. lower boundary is always 1
+    """
+    prime = []
+    sieve = [True] * (n + 1)
+    for p in range(2, n + 1):
+        if (sieve[p]):
+            prime.append(p)
+            for i in range(p, n + 1, p):
                 sieve[i] = False
-        p += 1
-    primes = [i for i in range(max_n + 1) if sieve[i]]
-    return primes
+    return prime
+
 
 def isWinner(x, nums):
-    if not nums or x <= 0:
+    """
+    Determines winner of Prime Game
+    Args:
+        x (int): no. of rounds of game
+        nums (int): upper limit of range for each round
+    Return:
+        Name of winner (Maria or Ben) or None if winner cannot be found
+    """
+    if x is None or nums is None or x == 0 or nums == []:
         return None
-
-    max_n = max(nums)
-    primes = sieve_of_eratosthenes(max_n)
-    maria_wins = 0
-    ben_wins = 0
-
-    for n in nums:
-        prime_count = sum(1 for p in primes if p <= n)
-        if prime_count % 2 == 0:
-            ben_wins += 1
+    Maria = Ben = 0
+    for i in range(x):
+        prime = primes(nums[i])
+        if len(prime) % 2 == 0:
+            Ben += 1
         else:
-            maria_wins += 1
-
-    if maria_wins > ben_wins:
-        return "Maria"
-    elif ben_wins > maria_wins:
-        return "Ben"
-    else:
-        return None
-
+            Maria += 1
+    if Maria > Ben:
+        return 'Maria'
+    elif Ben > Maria:
+        return 'Ben'
+    return None
